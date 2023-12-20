2ff83af7276b951662b1077d96986ddcbc7600f5
iqwrwq <iqwrwq@celthin.speedport.ip>
2023-12-20 22:56:23 +0100

    chore!: APP BASE

--- src/main/java/de/shopitech/mywish/Application.java
+++ src/main/java/de/shopitech/mywish/Application.java
@@ -1,19 +1,11 @@
 package de.shopitech.mywish;
 
-import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
-
-import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.sql.init.SqlDataSourceScriptDatabaseInitializer;
-import org.springframework.boot.autoconfigure.sql.init.SqlInitializationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * The entry point of the Spring Boot application.
